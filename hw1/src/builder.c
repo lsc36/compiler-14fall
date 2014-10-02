74360914589ca734e17a85b1fb8d1772ba96c3ce
Shao-Chuan Lee <lsc36x@gmail.com>
2014-10-02 17:06:10 +0800

    cleanup code, break down code into files

---
+++ hw1/src/builder.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include "header.h"
+
+
+/*********************************************************************
+  Build AST
+ **********************************************************************/
+Declaration makeDeclarationNode( Token declare_type, Token identifier )
+{
+    Declaration tree_node;
+
+    switch(declare_type.type){
+        case FloatDeclaration:
+            tree_node.type = Float;
+            break;
+        case IntegerDeclaration:
+            tree_node.type = Int;
+            break;
+        default:
+            break;
+    }
+    tree_node.name = identifier.tok[0];
+
+    return tree_node;
+}
+
+Declarations *makeDeclarationTree( Declaration decl, Declarations *decls )
+{
+    Declarations *new_tree = (Declarations *)malloc( sizeof(Declarations) );
+    new_tree->first = decl;
+    new_tree->rest = decls;
+
+    return new_tree;
+}
+
+
+Statement makeAssignmentNode( char id, Expression *v, Expression *expr_tail )
+{
+    Statement stmt;
+    AssignmentStatement assign;
+
+    stmt.type = Assignment;
+    assign.id = id;
+    if(expr_tail == NULL)
+        assign.expr = v;
+    else
+        assign.expr = expr_tail;
+    stmt.stmt.assign = assign;
+
+    return stmt;
+}
+
+Statement makePrintNode( char id )
+{
+    Statement stmt;
+    stmt.type = Print;
+    stmt.stmt.variable = id;
+
+    return stmt;
+}
+
+Statements *makeStatementTree( Statement stmt, Statements *stmts )
+{
+    Statements *new_tree = (Statements *)malloc( sizeof(Statements) );
+    new_tree->first = stmt;
+    new_tree->rest = stmts;
+
+    return new_tree;
+}
+
+
+/********************************************************
+  Build symbol table
+ *********************************************************/
+void InitializeTable( SymbolTable *table )
+{
+    int i;
+
+    for(i = 0 ; i < 26; i++)
+        table->table[i] = Notype;
+}
+
+void add_table( SymbolTable *table, char c, DataType t )
+{
+    int index = (int)(c - 'a');
+
+    if(table->table[index] != Notype)
+        printf("Error : id %c has been declared\n", c);//error
+    table->table[index] = t;
+}
+
+SymbolTable build( Program program )
+{
+    SymbolTable table;
+    Declarations *decls = program.declarations;
+    Declaration current;
+
+    InitializeTable(&table);
+
+    while(decls !=NULL){
+        current = decls->first;
+        add_table(&table, current.name, current.type);
+        decls = decls->rest;
+    }
+
+    return table;
+}
--- hw1/src/header.h
+++ hw1/src/header.h
@@ -1,16 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
 /******************************************************************************************************************************************
     All enumeration literals
        TokenType : Specify the type of the token scanner returns
-	   DataType  : The data type of the declared variable
-	   StmtType  : Indicate one statement in AcDc program is print or assignment statement.
-	   ValueType : The node types of the expression tree that represents the expression on the right hand side of the assignment statement.
-	               Identifier, IntConst, FloatConst must be the leaf nodes ex: a, b, c , 1.5 , 3.
-				   PlusNode, MinusNode, MulNode, DivNode are the operations in AcDc. They must be the internal nodes.
-                   Note that IntToFloatConvertNode to represent the type coercion may appear after finishing type checking. 			  
-	   Operation : Specify all arithematic expression, including +, - , *, / and type coercion.
+       DataType  : The data type of the declared variable
+       StmtType  : Indicate one statement in AcDc program is print or assignment statement.
+       ValueType : The node types of the expression tree that represents the expression on the right hand side of the assignment statement.
+                   Identifier, IntConst, FloatConst must be the leaf nodes ex: a, b, c , 1.5 , 3.
+                   PlusNode, MinusNode, MulNode, DivNode are the operations in AcDc. They must be the internal nodes.
+                   Note that IntToFloatConvertNode to represent the type coercion may appear after finishing type checking.
+       Operation : Specify all arithematic expression, including +, - , *, / and type coercion.
 *******************************************************************************************************************************************/
 
 typedef enum TokenType { FloatDeclaration, IntegerDeclaration, PrintOp, AssignmentOp, PlusOp, MinusOp,
@@ -21,8 +27,8 @@ typedef enum ValueType { Identifier, IntConst, FloatConst, PlusNode, MinusNode, 
 typedef enum Operation { Plus, Minus, Mul, Div, Assign, IntToFloatConvert } Operation;
 
 
-/**************************************************************************************** 
-   All structures to facilitate the processes of 
+/****************************************************************************************
+   All structures to facilitate the processes of
    scanning, parsing, AST, type-checking, building the symbol table, and code generation.
 *****************************************************************************************/
 
@@ -41,11 +47,11 @@ typedef struct Declaration{
     char name;
 }Declaration;
 
-/* 
+/*
     For decls production or say all declarations. (
-	You can view it as the subtree for decls in AST,
-	or just view it as the linked list that stores 
-	all declarations. ) 
+    You can view it as the subtree for decls in AST,
+    or just view it as the linked list that stores
+    all declarations. )
 */
 typedef struct Declarations{
     Declaration first;
@@ -64,10 +70,10 @@ typedef struct Value{
 }Value;
 
 
-/* 
+/*
    The data structure of the expression tree.
-   Recall how to deal with expression by tree 
-   in data structure course.   
+   Recall how to deal with expression by tree
+   in data structure course.
 */
 typedef struct Expression{
     Value v;
