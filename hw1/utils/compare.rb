f8fd2722e94345786b4537c7e0e59a613e1eb089
MROS <yc1043@gmail.com>
2014-10-07 13:11:35 +0800

    improve test script output message

--- hw1/utils/compare.rb
+++ hw1/utils/compare.rb
@@ -6,6 +6,7 @@ def expand_test_dir(str)
   File.expand_path(File.join(TEST_SUITE_DIR, str), __FILE__)
 end
 
+error = 0
 (1..TEST_NUMBER).each do |i|
   input = expand_test_dir("sample" + i.to_s + ".ac")
   output = expand_test_dir("myAnswer" + i.to_s + ".dc")
@@ -17,7 +18,11 @@ end
   diff = `diff #{output} #{answer}`
 
   if diff != ""
+    error += 1
     puts "test number #{i} is wrong!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     puts diff
   end
 end
+puts "****************************************************"
+puts "#{error} errors occur, total #{TEST_NUMBER} test case"
+puts "****************************************************"
