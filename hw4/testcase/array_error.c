bde04049031ee5d178d08cde8d4b227cc45c95cc
MROS <yc1043@gmail.com>
2014-12-18 17:46:10 +0800

    in testcase, add semicolon, remove unsopported comment

--- hw4/testcase/array_error.c
+++ hw4/testcase/array_error.c
@@ -6,12 +6,12 @@ int main() {
 	int a[10][10];
 	int b = a[3][2];
 	b = a[2.1][4];
-	b = a[2.1 * 4 - 2][4]
+	b = a[2.1 * 4 - 2][4];
 	b = a[3];
 	b = a[1][1][1];
 
-	func(a, b); // correct
-	func(a[1], b); // correct
-	func(a, a[1][1]); // correct
-	func(a[1][1], a[1][1]); // incorrect
+	func(a, b); 
+	func(a[1], b); 
+	func(a, a[1][1]);
+	func(a[1][1], a[1][1]);
 }
