1b27c7fd14c339be60a48ae203a75125e4d4184d
Shao-Chuan Lee <lsc36x@gmail.com>
2015-01-06 21:38:30 +0800

    genFunctionDecl

--- hw5/src/codegen.c
+++ hw5/src/codegen.c
@@ -1,5 +1,7 @@
 #include "codegen.h"
 
+#define IDSTR(idNode) (idNode->semantic_value.identifierSemanticValue.identifierName)
+
 FILE *outfile;
 
 void codegenInit() {
@@ -14,6 +16,7 @@ void emit(char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
     vfprintf(outfile, fmt, args);
+    fprintf(outfile, "\n");
     va_end(args);
 }
 
@@ -21,8 +24,31 @@ void genGlobalVarDecl(AST_NODE *varDeclListNode) {
     // TODO
 }
 
-void genFunctionDecl(AST_NODE *funcDeclNode) {
+void genBlock(AST_NODE *blockNode) {
     // TODO
+}
+
+void genFunctionDecl(AST_NODE *funcDeclNode) {
+    AST_NODE *typeNode, *idNode, *paramListNode, *blockNode;
+    typeNode = funcDeclNode->child;
+    idNode = typeNode->rightSibling;
+    paramListNode = idNode->rightSibling;
+    blockNode = paramListNode->rightSibling;
+
+    emit(".text");
+    emit("_start_%s:", IDSTR(idNode));
+    emit("str lr, [sp, #0]");
+    emit("str fp, [sp, #-4]");
+    emit("add fp, sp, #-4");
+    emit("add sp, sp, #-8");
+
+    genBlock(blockNode);
+
+    emit("_end_%s:", IDSTR(idNode));
+    emit("ldr lr, [fp, #4]");
+    emit("add sp, fp, #4");
+    emit("ldr fp, [fp, #0]");
+    emit("bx lr");
 }
 
 void codegen(AST_NODE *root) {
