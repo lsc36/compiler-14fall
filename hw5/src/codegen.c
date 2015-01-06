0f9968254a3c5bd3ba14c2f9ce0abf7ccaeeb63d
Shao-Chuan Lee <lsc36x@gmail.com>
2015-01-06 23:31:05 +0800

    calculate frame size before codegen, callee save regs

--- hw5/src/codegen.c
+++ hw5/src/codegen.c
@@ -1,11 +1,5 @@
 #include "codegen.h"
 
-#define IDSTR(idNode) (idNode->semantic_value.identifierSemanticValue.identifierName)
-#define IDKIND(idNode) (idNode->semantic_value.identifierSemanticValue.kind)
-#define IDSYM(idNode) (idNode->semantic_value.identifierSemanticValue.symbolTableEntry)
-#define DECLKIND(declNode) (declNode->semantic_value.declSemanticValue.kind)
-#define SYMARRPROP(symtabEntry) (symtabEntry->attribute->attr.typeDescriptor->properties.arrayProperties)
-
 FILE *outfile;
 
 void codegenInit() {
@@ -34,7 +28,30 @@ void genStmtList(AST_NODE *stmtListNode) {
 
 void genBlock(AST_NODE *blockNode) {
     AST_NODE *child = blockNode->child;
-    int stackSize = 0;
+    if (child != NULL && child->nodeType == VARIABLE_DECL_LIST_NODE) {
+        AST_NODE *declNode = child->child;
+        for (; declNode != NULL; declNode = declNode->rightSibling) {
+            if (DECLKIND(declNode) == VARIABLE_DECL) {
+                AST_NODE *typeNode, *idNode;
+                typeNode = declNode->child;
+                idNode = typeNode->rightSibling;
+                for (; idNode != NULL; idNode = idNode->rightSibling) {
+                    if (IDKIND(idNode) == WITH_INIT_ID) {
+                        // TODO assign init value
+                    }
+                }
+            }
+        }
+        child = child->rightSibling;
+    }
+    if (child != NULL) {
+        genStmtList(child);
+    }
+}
+
+int calcBlockOffset(AST_NODE *blockNode) {
+    AST_NODE *child = blockNode->child;
+    int baseSize = 0, innerSize = 0;
     if (child != NULL && child->nodeType == VARIABLE_DECL_LIST_NODE) {
         AST_NODE *declNode = child->child;
         for (; declNode != NULL; declNode = declNode->rightSibling) {
@@ -47,17 +64,16 @@ void genBlock(AST_NODE *blockNode) {
                     int product = 1, i;
                     switch (IDKIND(idNode)) {
                     case NORMAL_ID:
-                        stackSize += 4;
+                        baseSize += 4;
                         break;
                     case ARRAY_ID:
                         for (i = 0; i < SYMARRPROP(IDSYM(idNode)).dimension; i++) {
                             product *= SYMARRPROP(IDSYM(idNode)).sizeInEachDimension[i];
                         }
-                        stackSize += 4 * product;
+                        baseSize += 4 * product;
                         break;
                     case WITH_INIT_ID:
-                        // TODO assign init value
-                        stackSize += 4;
+                        baseSize += 4;
                         break;
                     default:
                         ;
@@ -68,17 +84,45 @@ void genBlock(AST_NODE *blockNode) {
                 ;
             }
         }
-        if (stackSize > 0) {
-            emit("add sp, sp, #-%d", stackSize);
-        }
         child = child->rightSibling;
     }
     if (child != NULL) {
-        genStmtList(child);
+        child = child->child;
+        for (; child != NULL; child = child->rightSibling) {
+            int tmp = 0, tmp2;
+            if (child->nodeType == STMT_NODE) {
+                switch (STMTKIND(child)) {
+                case WHILE_STMT:
+                    if (child->child->rightSibling->nodeType == BLOCK_NODE) {
+                        tmp = calcBlockOffset(child->child->rightSibling);
+                    }
+                    break;
+                case FOR_STMT:
+                    if (child->child->rightSibling->rightSibling->rightSibling->nodeType == BLOCK_NODE) {
+                        tmp = calcBlockOffset(child->child->rightSibling->rightSibling->rightSibling);
+                    }
+                    break;
+                case IF_STMT:
+                    if (child->child->rightSibling->nodeType == BLOCK_NODE) {
+                        tmp = calcBlockOffset(child->child->rightSibling);
+                    }
+                    if (child->child->rightSibling->rightSibling->nodeType == BLOCK_NODE) {
+                        tmp2 = calcBlockOffset(child->child->rightSibling->rightSibling);
+                    }
+                    if (tmp < tmp2) tmp = tmp2;
+                    break;
+                default:
+                    ;
+                }
+            } else if (child->nodeType == BLOCK_NODE) {
+                tmp = calcBlockOffset(child);
+            } else if (child->nodeType == NUL_NODE) {
+                // do nothing
+            }
+            if (innerSize < tmp) innerSize = tmp;
+        }
     }
-    if (stackSize > 0) {
-        emit("add sp, sp, #%d", stackSize);
-    }
+    return baseSize + innerSize;
 }
 
 void genFunctionDecl(AST_NODE *funcDeclNode) {
@@ -92,12 +136,46 @@ void genFunctionDecl(AST_NODE *funcDeclNode) {
     emit("_start_%s:", IDSTR(idNode));
     emit("str lr, [sp, #0]");
     emit("str fp, [sp, #-4]");
-    emit("add fp, sp, #-4");
-    emit("add sp, sp, #-8");
+    emit("sub fp, sp, #4");
+
+    int frameSize = calcBlockOffset(blockNode);
+    emit("sub sp, fp, #%d", 4 + frameSize + 64);
+    emit("str r4, [sp, #4]");
+    emit("str r5, [sp, #8]");
+    emit("str r6, [sp, #12]");
+    emit("str r7, [sp, #16]");
+    emit("str r8, [sp, #20]");
+    emit("str r9, [sp, #24]");
+    emit("str r10, [sp, #28]");
+    emit("str r11, [sp, #32]");
+    emit("vstr.f32 s16, [sp, #36]");
+    emit("vstr.f32 s17, [sp, #40]");
+    emit("vstr.f32 s18, [sp, #44]");
+    emit("vstr.f32 s19, [sp, #48]");
+    emit("vstr.f32 s20, [sp, #52]");
+    emit("vstr.f32 s21, [sp, #56]");
+    emit("vstr.f32 s22, [sp, #60]");
+    emit("vstr.f32 s23, [sp, #64]");
 
     genBlock(blockNode);
 
     emit("_end_%s:", IDSTR(idNode));
+    emit("ldr r4, [sp, #4]");
+    emit("ldr r5, [sp, #8]");
+    emit("ldr r6, [sp, #12]");
+    emit("ldr r7, [sp, #16]");
+    emit("ldr r8, [sp, #20]");
+    emit("ldr r9, [sp, #24]");
+    emit("ldr r10, [sp, #28]");
+    emit("ldr r11, [sp, #32]");
+    emit("vldr.f32 s16, [sp, #36]");
+    emit("vldr.f32 s17, [sp, #40]");
+    emit("vldr.f32 s18, [sp, #44]");
+    emit("vldr.f32 s19, [sp, #48]");
+    emit("vldr.f32 s20, [sp, #52]");
+    emit("vldr.f32 s21, [sp, #56]");
+    emit("vldr.f32 s22, [sp, #60]");
+    emit("vldr.f32 s23, [sp, #64]");
     emit("ldr lr, [fp, #4]");
     emit("add sp, fp, #4");
     emit("ldr fp, [fp, #0]");
--- hw5/src/codegen.h
+++ hw5/src/codegen.h
@@ -6,6 +6,21 @@
 #include "header.h"
 #include "symbolTable.h"
 
+#define IDSTR(idNode) (idNode->semantic_value.identifierSemanticValue.identifierName)
+#define IDKIND(idNode) (idNode->semantic_value.identifierSemanticValue.kind)
+#define IDSYM(idNode) (idNode->semantic_value.identifierSemanticValue.symbolTableEntry)
+#define DECLKIND(declNode) (declNode->semantic_value.declSemanticValue.kind)
+#define STMTKIND(stmtNode) (stmtNode->semantic_value.stmtSemanticValue.kind)
+#define SYMARRPROP(symtabEntry) (symtabEntry->attribute->attr.typeDescriptor->properties.arrayProperties)
+
+void emit(char *fmt, ...);
+int calcBlockOffset(AST_NODE *blockNode);
+
+void genGlobalVarDecl(AST_NODE *varDeclListNode);
+void genStmtList(AST_NODE *stmtListNode);
+void genBlock(AST_NODE *blockNode);
+void genFunctionDecl(AST_NODE *funcDeclNode);
+
 void codegen(AST_NODE *root);
 
 #endif
